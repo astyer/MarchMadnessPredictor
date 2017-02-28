47ef49c8f50642bc53e1528021d8a2d9a2ab6985
astyer <88atti@gmail.com>
2017-02-28 10:33:01 -0500

    started

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,27 @@
 package com.company;
 
+import java.io.*;
+import java.util.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
 
-        
+        Scanner sf = new Scanner(new File("2016seeds.txt"));
 
+        int maxIndex = -1;
+        String data [] = new String [1000];
+        while(sf.hasNextLine())
+        {
+            maxIndex++;
+            data[maxIndex] = sf.nextLine();
+        }
+        sf.close();
+
+        for(int i = 0; i < data.length; i++)
+        {
+            String toSplit = data[i];
+            String
+        }
     }
 }
