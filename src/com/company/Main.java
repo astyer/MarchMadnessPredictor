4723f19e668676a87cb8dd3c65959ab3b855344e
astyer <88atti@gmail.com>
2017-03-15 11:15:21 -0400

    in progress still still still still still still still still

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,3 +1,6 @@
+// providence/USC
+// N.C. Central/UC Davis
+
 package com.company;
 
 import javax.imageio.ImageIO;
@@ -6,7 +9,6 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.*;
-
 public class Main {
 
     static BufferedImage newImage;
@@ -36,7 +38,7 @@ public class Main {
         mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         mainFrame.setVisible(true);
 
-        Scanner sf = new Scanner(new File("2016data.txt"));
+        Scanner sf = new Scanner(new File("2017data.txt"));
 
         ArrayList data = new ArrayList<String>();
         while(sf.hasNextLine())
@@ -56,134 +58,134 @@ public class Main {
             teams.add(a);
         }
 
-        ArrayList south = new ArrayList();
+        ArrayList W = new ArrayList();
         for(int i = 0; i < 16; i++)
         {
-            south.add(teams.get(i));
+            W.add(teams.get(i));
         }
 
-        ArrayList west = new ArrayList();
+        ArrayList X = new ArrayList();
         for(int i = 16; i < 32; i++)
         {
-            west.add(teams.get(i));
+            X.add(teams.get(i));
         }
 
-        ArrayList east = new ArrayList();
+        ArrayList Y = new ArrayList();
         for(int i = 32; i < 48; i++)
         {
-            east.add(teams.get(i));
+            Y.add(teams.get(i));
         }
 
-        ArrayList midwest = new ArrayList();
+        ArrayList Z = new ArrayList();
         for(int i = 48; i < 64; i++)
         {
-            midwest.add(teams.get(i));
+            Z.add(teams.get(i));
         }
 
         System.out.println("March Madness Predictions:");
 
-        drawTeams(south, g2d, 100, 35, 26);
-        nextRound(south);
-        System.out.println("Round of 32 South:");
-        printTeams(south);
-        drawTeams(south, g2d, 295, 50, 52);
+        drawTeams(W, g2d, 100, 35, 26);
+        nextRound(W);
+        System.out.println("Round of 32 W:");
+        printTeams(W);
+        drawTeams(W, g2d, 295, 50, 52);
         System.out.println("");
-        nextRound(south);
-        System.out.println("Round of 16 South:");
-        printTeams(south);
-        drawTeams(south, g2d, 440, 75, 105);
+        nextRound(W);
+        System.out.println("Round of 16 W:");
+        printTeams(W);
+        drawTeams(W, g2d, 440, 75, 105);
         System.out.println("");
-        nextRound(south);
-        System.out.println("Quarterfinals South:");
-        printTeams(south);
-        drawTeams(south, g2d, 560, 127, 210);
+        nextRound(W);
+        System.out.println("Quarterfinals W:");
+        printTeams(W);
+        drawTeams(W, g2d, 560, 127, 210);
         System.out.println("");
-        nextRound(south);
-        System.out.println("Semifinals South:");
-        printTeams(south);
-        drawTeams(south, g2d, 675, 233, 0);
-        System.out.println("");
-
-        drawTeams(west, g2d, 100, 460, 26);
-        nextRound(west);
-        System.out.println("Round of 32 West:");
-        printTeams(west);
-        drawTeams(west, g2d, 295, 475, 52);
-        System.out.println("");
-        nextRound(west);
-        System.out.println("Round of 16 West:");
-        printTeams(west);
-        drawTeams(west, g2d, 440, 500, 105);
-        System.out.println("");
-        nextRound(west);
-        System.out.println("Quarterfinals West:");
-        printTeams(west);
-        drawTeams(west, g2d, 560, 552, 210);
-        System.out.println("");
-        nextRound(west);
-        System.out.println("Semifinals West:");
-        printTeams(west);
-        drawTeams(west, g2d, 675, 658, 0);
+        nextRound(W);
+        System.out.println("Semifinals W:");
+        printTeams(W);
+        drawTeams(W, g2d, 675, 233, 0);
         System.out.println("");
 
-        drawTeams(east, g2d, 1630, 35, 26);
-        nextRound(east);
-        System.out.println("Round of 32 East:");
-        printTeams(east);
-        drawTeams(east, g2d, 1444, 50, 52);
+        drawTeams(X, g2d, 100, 460, 26);
+        nextRound(X);
+        System.out.println("Round of 32 X:");
+        printTeams(X);
+        drawTeams(X, g2d, 295, 475, 52);
         System.out.println("");
-        nextRound(east);
-        System.out.println("Round of 16 East:");
-        printTeams(east);
-        drawTeams(east, g2d, 1323, 75, 105);
+        nextRound(X);
+        System.out.println("Round of 16 X:");
+        printTeams(X);
+        drawTeams(X, g2d, 440, 500, 105);
         System.out.println("");
-        nextRound(east);
-        System.out.println("Quarterfinals East:");
-        printTeams(east);
-        drawTeams(east, g2d, 1205, 127, 210);
+        nextRound(X);
+        System.out.println("Quarterfinals X:");
+        printTeams(X);
+        drawTeams(X, g2d, 560, 552, 210);
         System.out.println("");
-        nextRound(east);
-        System.out.println("Semifinals East:");
-        printTeams(east);
-        drawTeams(east, g2d, 1090, 233, 0);
+        nextRound(X);
+        System.out.println("Semifinals X:");
+        printTeams(X);
+        drawTeams(X, g2d, 675, 658, 0);
         System.out.println("");
 
-        drawTeams(midwest, g2d, 1630, 460, 26);
-        nextRound(midwest);
-        System.out.println("Round of 32 Midwest:");
-        printTeams(midwest);
-        drawTeams(midwest, g2d, 1444, 475, 52);
+        drawTeams(Y, g2d, 1630, 35, 26);
+        nextRound(Y);
+        System.out.println("Round of 32 Y:");
+        printTeams(Y);
+        drawTeams(Y, g2d, 1444, 50, 52);
         System.out.println("");
-        nextRound(midwest);
-        System.out.println("Round of 16 Midwest:");
-        printTeams(midwest);
-        drawTeams(midwest, g2d, 1323, 500, 105);
+        nextRound(Y);
+        System.out.println("Round of 16 Y:");
+        printTeams(Y);
+        drawTeams(Y, g2d, 1323, 75, 105);
         System.out.println("");
-        nextRound(midwest);
-        System.out.println("Quarterfinals Midwest:");
-        printTeams(midwest);
-        drawTeams(midwest, g2d, 1205, 552, 210);
+        nextRound(Y);
+        System.out.println("Quarterfinals Y:");
+        printTeams(Y);
+        drawTeams(Y, g2d, 1205, 127, 210);
         System.out.println("");
-        nextRound(midwest);
-        System.out.println("Semifinals Midwest:");
-        printTeams(midwest);
-        drawTeams(midwest, g2d, 1090, 658, 0);
+        nextRound(Y);
+        System.out.println("Semifinals Y:");
+        printTeams(Y);
+        drawTeams(Y, g2d, 1090, 233, 0);
+        System.out.println("");
+
+        drawTeams(Z, g2d, 1630, 460, 26);
+        nextRound(Z);
+        System.out.println("Round of 32 Z:");
+        printTeams(Z);
+        drawTeams(Z, g2d, 1444, 475, 52);
+        System.out.println("");
+        nextRound(Z);
+        System.out.println("Round of 16 Z:");
+        printTeams(Z);
+        drawTeams(Z, g2d, 1323, 500, 105);
+        System.out.println("");
+        nextRound(Z);
+        System.out.println("Quarterfinals Z:");
+        printTeams(Z);
+        drawTeams(Z, g2d, 1205, 552, 210);
+        System.out.println("");
+        nextRound(Z);
+        System.out.println("Semifinals Z:");
+        printTeams(Z);
+        drawTeams(Z, g2d, 1090, 658, 0);
         System.out.println("");
 
         ArrayList leftsemis = new ArrayList();
-        leftsemis.add(south.get(0));
-        leftsemis.add(west.get(0));
+        leftsemis.add(W.get(0));
+        leftsemis.add(X.get(0));
         nextRound(leftsemis);
-        System.out.println("South and West finalist:");
+        System.out.println("W and X finalist:");
         printTeams(leftsemis);
         drawTeams(leftsemis, g2d, 686, 455, 0);
         System.out.println("");
 
         ArrayList rightsemis = new ArrayList();
-        rightsemis.add(east.get(0));
-        rightsemis.add(midwest.get(0));
+        rightsemis.add(Y.get(0));
+        rightsemis.add(Z.get(0));
         nextRound(rightsemis);
-        System.out.println("East and Midwest finalist:");
+        System.out.println("Y and Z finalist:");
         printTeams(rightsemis);
         drawTeams(rightsemis, g2d, 1052, 455, 0);
         System.out.println("");
